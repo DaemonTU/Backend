2e490e9f62a6ab5a7b79400e94c763a968c33a62
hrk2023 <hrkdasgupta025@gmail.com>
2020-11-20 19:27:20 +0530

    News Api Route Added in dataRoutes.py file

--- api/routes/dataRoutes.py
+++ api/routes/dataRoutes.py
@@ -1,4 +1,5 @@
 from flask import request,jsonify,Blueprint, redirect,url_for, flash
+import requests
 def construct_blueprint(cluster):
     data = Blueprint("data",__name__,url_prefix="/api")
 
@@ -72,5 +73,38 @@ def construct_blueprint(cluster):
                 return redirect(url_for('admin.add_members'))
             flash('Member cannot be added','error')
             return redirect(url_for('admin.add_members'))
+
+    @data.route("/news",methods=["GET"])     #FETCH NEWS
+    def get_news():
+        url = "https://techcrunch.com/wp-json/tc/v1/magazine?page=1&_embed=true&cachePrevention=0"
+        headers={
+            'accept-language': 'en-US,en;q=0.9',
+            'content-type': 'application/json; charset=utf-8',
+            'referer': 'https://techcrunch.com/',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Mobile Safari/537.36',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-User': '?1',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Mobile Safari/537.3'
+        }
+        response = requests.get(url,headers=headers)
+        if response.status_code == 200:
+            result = response.json()
+            output = []
+            for response in result: 
+                desc = response["excerpt"]["rendered"]
+                desc = desc[3:-5]
+                data = {
+                    "title" : response["title"]["rendered"],
+                    "description" : desc,
+                    "poster" : response["jetpack_featured_media_url"],
+                    "original_link" : response["link"]
+                }
+                output.append(data)
+            return jsonify({"status" : 200,"result" : output})
+        return jsonify({"status" : 500,"message" : "Internal Server Error"})
 
     return data
